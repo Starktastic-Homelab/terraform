f19039391f0ade52c74961ea9d4aa56ab6ae6cc9
Ben Faingold <benfaingold@microsoft.com>
2025-07-21 19:02:30 +0300

    Remove outputs.tf (probably temporarily)

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "master_default_ipv4_addresses" {
-  value = module.master_nodes[*].default_ipv4_address
-}
-
-output "worker_default_ipv4_addresses" {
-  value = module.worker_nodes[*].default_ipv4_address
-}
