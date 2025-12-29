63fd3b6422749e71dca55e1dd07499b300a97e6a
Ben Faingold <benfaingold@microsoft.com>
2025-12-29 18:15:35 +0200

    Switch to pcis block

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -14,8 +14,8 @@ resource "proxmox_vm_qemu" "vm" {
   boot               = "order=virtio0"
   start_at_node_boot = true
   agent              = 1
-  machine     = var.machine
-  bios        = var.bios
+  machine            = var.machine
+  bios               = var.bios
 
   cpu {
     sockets = 1
@@ -78,13 +78,167 @@ resource "proxmox_vm_qemu" "vm" {
         }
       }
     }
+  }
 
-    dynamic "hostpci" {
-      for_each = var.pci_devices
+  pcis {
+    dynamic "pci0" {
+      for_each = length(var.pci_devices) > 0 ? [var.pci_devices[0]] : []
       content {
-        host   = hostpci.value.host  
-        rombar = try(hostpci.value.rombar, 1)
-        pcie   = try(hostpci.value.pcie, 0)
+        raw {
+          raw_id = pci0.value.host
+          pcie   = try(pci0.value.pcie, false)
+          rombar = try(pci0.value.rombar, true)
+        }
+      }
+    }
+    dynamic "pci1" {
+      for_each = length(var.pci_devices) > 1 ? [var.pci_devices[1]] : []
+      content {
+        raw {
+          raw_id = pci1.value.host
+          pcie   = try(pci1.value.pcie, false)
+          rombar = try(pci1.value.rombar, true)
+        }
+      }
+    }
+    dynamic "pci2" {
+      for_each = length(var.pci_devices) > 2 ? [var.pci_devices[2]] : []
+      content {
+        raw {
+          raw_id = pci2.value.host
+          pcie   = try(pci2.value.pcie, false)
+          rombar = try(pci2.value.rombar, true)
+        }
+      }
+    }
+    dynamic "pci3" {
+      for_each = length(var.pci_devices) > 3 ? [var.pci_devices[3]] : []
+      content {
+        raw {
+          raw_id = pci3.value.host
+          pcie   = try(pci3.value.pcie, false)
+          rombar = try(pci3.value.rombar, true)
+        }
+      }
+    }
+    dynamic "pci4" {
+      for_each = length(var.pci_devices) > 4 ? [var.pci_devices[4]] : []
+      content {
+        raw {
+          raw_id = pci4.value.host
+          pcie   = try(pci4.value.pcie, false)
+          rombar = try(pci4.value.rombar, true)
+        }
+      }
+    }
+    dynamic "pci5" {
+      for_each = length(var.pci_devices) > 5 ? [var.pci_devices[5]] : []
+      content {
+        raw {
+          raw_id = pci5.value.host
+          pcie   = try(pci5.value.pcie, false)
+          rombar = try(pci5.value.rombar, true)
+        }
+      }
+    }
+    dynamic "pci6" {
+      for_each = length(var.pci_devices) > 6 ? [var.pci_devices[6]] : []
+      content {
+        raw {
+          raw_id = pci6.value.host
+          pcie   = try(pci6.value.pcie, false)
+          rombar = try(pci6.value.rombar, true)
+        }
+      }
+    }
+    dynamic "pci7" {
+      for_each = length(var.pci_devices) > 7 ? [var.pci_devices[7]] : []
+      content {
+        raw {
+          raw_id = pci7.value.host
+          pcie   = try(pci7.value.pcie, false)
+          rombar = try(pci7.value.rombar, true)
+        }
+      }
+    }
+    dynamic "pci8" {
+      for_each = length(var.pci_devices) > 8 ? [var.pci_devices[8]] : []
+      content {
+        raw {
+          raw_id = pci8.value.host
+          pcie   = try(pci8.value.pcie, false)
+          rombar = try(pci8.value.rombar, true)
+        }
+      }
+    }
+    dynamic "pci9" {
+      for_each = length(var.pci_devices) > 9 ? [var.pci_devices[9]] : []
+      content {
+        raw {
+          raw_id = pci9.value.host
+          pcie   = try(pci9.value.pcie, false)
+          rombar = try(pci9.value.rombar, true)
+        }
+      }
+    }
+    dynamic "pci10" {
+      for_each = length(var.pci_devices) > 10 ? [var.pci_devices[10]] : []
+      content {
+        raw {
+          raw_id = pci10.value.host
+          pcie   = try(pci10.value.pcie, false)
+          rombar = try(pci10.value.rombar, true)
+        }
+      }
+    }
+    dynamic "pci11" {
+      for_each = length(var.pci_devices) > 11 ? [var.pci_devices[11]] : []
+      content {
+        raw {
+          raw_id = pci11.value.host
+          pcie   = try(pci11.value.pcie, false)
+          rombar = try(pci11.value.rombar, true)
+        }
+      }
+    }
+    dynamic "pci12" {
+      for_each = length(var.pci_devices) > 12 ? [var.pci_devices[12]] : []
+      content {
+        raw {
+          raw_id = pci12.value.host
+          pcie   = try(pci12.value.pcie, false)
+          rombar = try(pci12.value.rombar, true)
+        }
+      }
+    }
+    dynamic "pci13" {
+      for_each = length(var.pci_devices) > 13 ? [var.pci_devices[13]] : []
+      content {
+        raw {
+          raw_id = pci13.value.host
+          pcie   = try(pci13.value.pcie, false)
+          rombar = try(pci13.value.rombar, true)
+        }
+      }
+    }
+    dynamic "pci14" {
+      for_each = length(var.pci_devices) > 14 ? [var.pci_devices[14]] : []
+      content {
+        raw {
+          raw_id = pci14.value.host
+          pcie   = try(pci14.value.pcie, false)
+          rombar = try(pci14.value.rombar, true)
+        }
+      }
+    }
+    dynamic "pci15" {
+      for_each = length(var.pci_devices) > 15 ? [var.pci_devices[15]] : []
+      content {
+        raw {
+          raw_id = pci15.value.host
+          pcie   = try(pci15.value.pcie, false)
+          rombar = try(pci15.value.rombar, true)
+        }
       }
     }
   }
