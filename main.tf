1557a85ce1dc5390a979769225b42ec883262208
Ben Faingold <benfaingold@microsoft.com>
2026-02-25 23:16:03 +0200

    Assign USB device to all workers

--- main.tf
+++ main.tf
@@ -73,10 +73,10 @@ module "worker_nodes" {
     rombar = false
   }]
 
-  usb_devices = count.index == 0 ? [{
+  usb_devices = [{
     mapping_id = "k3s-worker-zigbee"
     usb3       = false
-  }] : []
+  }]
 
   tags = "k3s,worker"
 }
